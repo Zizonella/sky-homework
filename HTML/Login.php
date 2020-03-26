b0060f302625709660d22ab712d29a9f59baf7d4
AfrodytaP <59373204+AfrodytaP@users.noreply.github.com>
2020-03-26 18:00:20 +0000

    Add files via upload

--- HTML/Login.php
+++ HTML/Login.php
@@ -2,7 +2,7 @@
 
 <html>
     <head>
-        <title></title>
+        <title>Login Page</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
@@ -12,60 +12,66 @@
         <link href="css.css" rel="stylesheet" type="text/css"/>
     </head>
     <body>
-        
         <div class="container">
-             <div class="row h-25 d-inline-block"></div>
-             <div class="row">
-                 <div class="col-md-7">
-             <ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
-              <li class="nav-item">
-                <a class="nav-link active" id="pills-home-tab" data-toggle="pill" href="#pills-home" role="tab" aria-controls="pills-home" aria-selected="true">Home</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" id="pills-profile-tab" data-toggle="pill" href="#pills-profile" role="tab" aria-controls="pills-profile" aria-selected="false">About us</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" id="pills-contact-tab" data-toggle="pill" href="#pills-contact" role="tab" aria-controls="pills-contact" aria-selected="false">Contact us</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" id="pills-contact-tab" data-toggle="pill" href="#pills-contact" role="tab" aria-controls="pills-contact" aria-selected="false">Login</a>
-              </li>
+           <div class="row h-25 d-inline-block"></div>
+           <div class="row">
+               <div class="col-md-7">
+                   <ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
+                       <li class="nav-item">
+                           <a class="nav-link active" id="pills-home-tab" data-toggle="pill" href="#pills-home" role="tab"
+                              aria-controls="pills-home" aria-selected="true">Home</a>
+                       </li>
+                       <li class="nav-item">
+                           <a class="nav-link" id="pills-profile-tab" data-toggle="pill" href="#pills-profile" role="tab"
+                              aria-controls="pills-profile" aria-selected="false">About us</a>
+                       </li>
+                       <li class="nav-item">
+                           <a class="nav-link" id="pills-contact-tab" data-toggle="pill" href="#pills-contact" role="tab"
+                              aria-controls="pills-contact" aria-selected="false">Contact us</a>
+                       </li>
+                       <li class="nav-item">
+                           <a class="nav-link" id="pills-contact-tab" data-toggle="pill" href="#pills-contact" role="tab"
+                              aria-controls="pills-contact" aria-selected="false">Login</a>
+                       </li>
+                   </ul>
+               </div>
+
+               <div class="col-md-5">
+                   <a href="Homepage.php"><img src="images/logo.png" width="400px" alt="Logo"/></a>
+               </div>
+
+           </div>
+       </div>
+        <div class="container">
+            <div class="container">
+              <div class="row">
+                <div class="col-sm-9 col-md-7 col-lg-5 mx-auto">
+                  <div class="card card-signin my-5">
+                    <div class="card-body">
+                      <h5 class="card-title text-center">Sign In</h5>
+                      <form class="form-signin">
+                        <div class="form-label-group">
+                          <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
+                          <label for="inputEmail">Email address</label>
+                        </div>
+
+                        <div class="form-label-group">
+                          <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
+                          <label for="inputPassword">Password</label>
+                        </div>
+
+                        <div class="custom-control custom-checkbox mb-3">
+                          <input type="checkbox" class="custom-control-input" id="customCheck1">
+                          <label class="custom-control-label" for="customCheck1">Remember password</label>
+                        </div>
+                        <button class="btn btn-lg btn-primary btn-block text-uppercase" type="submit">Sign in</button>
+                      </form>
+                    </div>
+                  </div>
+                </div>
               </div>
-              <div class="col-md-5">
-                <a href="Homepage.php"><img src="images/logo.png" max-width="400px" alt="Logo"/></a>
-            </ul>
-             </div>  
             </div>
-             
-  <div class="container">
-    <div class="row">
-      <div class="col-sm-9 col-md-7 col-lg-5 mx-auto">
-        <div class="card card-signin my-5">
-          <div class="card-body">
-            <h5 class="card-title text-center">Sign In</h5>
-            <form class="form-signin">
-              <div class="form-label-group">
-                <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
-                <label for="inputEmail">Email address</label>
-              </div>
-
-              <div class="form-label-group">
-                <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
-                <label for="inputPassword">Password</label>
-              </div>
-
-              <div class="custom-control custom-checkbox mb-3">
-                <input type="checkbox" class="custom-control-input" id="customCheck1">
-                <label class="custom-control-label" for="customCheck1">Remember password</label>
-              </div>
-              <button class="btn btn-lg btn-primary btn-block text-uppercase" type="submit">Sign in</button>
-            </form>
-          </div>
         </div>
-      </div>
-    </div>
-  </div>
-</div>
 </body>
     </body>
 </html>
